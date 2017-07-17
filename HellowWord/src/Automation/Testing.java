7aecc7044ba365eb310fc88f0162d36719baf598
RofhiwaMarx <marx.tshivhasa@olg.co.za>
2017-07-17 12:26:43 +0200

    Commiting the 2nd class to remote git

---
+++ HellowWord/src/Automation/Testing.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package Automation;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Marx.Tshivhasa
+ *
+ */
+public class Testing {
+
+	String fruit = "Apple";
+	
+	@Test
+	public void test() {
+		
+	try {
+		System.out.println(fruit);
+	} catch (Exception e) {
+
+		fail("Not yet implemented");
+	}
+		
+		
+	}
+
+}
